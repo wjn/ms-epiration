84191d4ce7fd71cb61434a68eedcbdf6f2bfe136
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-11-01 08:52:10 -0600

    initial service setup, containerization and k8s setup is in the infra dir

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { NotFoundError, logIt, LogType, natsWrapper } from '@nielsendigital/ms-common';
+
+const startApp = async () => {
+  logIt.out(LogType.STARTED, 'Expiration service started');
+  // verify env vars are present
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new NotFoundError('NATS_CLUSTER_ID k8s env var must be defined.');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new NotFoundError('NATS_CLIENT_ID k8s env var must be defined.');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new NotFoundError('NATS_URL k8s env var must be defined.');
+  }
+
+  logIt.out(LogType.INFO, 'All required ENV Vars verified as defined');
+
+  // connect to NATS
+  try {
+    logIt.out(LogType.INFO, 'Attempting connection to NATS');
+
+    await natsWrapper.connect(
+      // see infra/k8s/expiration-depl.yaml for values
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    // process graceful exit
+    natsWrapper.client.on('close', () => {
+      logIt.out(LogType.STOPPED, 'NATS Connection Closed');
+      process.exit();
+    });
+
+    // gracefully exit
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+    // -----
+  } catch (err) {
+    logIt.out(LogType.ERROR, 'NATS failed to load.');
+    logIt.out(LogType.ERROR, err);
+  }
+};
+
+startApp();
